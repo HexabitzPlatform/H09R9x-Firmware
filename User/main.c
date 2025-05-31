8de14c317390e74590f881b1680cb5bc1404028d
Mahmood_Reda <mr4141705@gmail.com>
2025-05-31 15:34:49 +0300

    clear topology & main files .

--- User/main.c
+++ User/main.c
@@ -1,56 +1,38 @@
 /*
- BitzOS (BOS) V0.3.6 - Copyright (C) 2017-2024 Hexabitz
+ BitzOS (BOS) V0.4.0 - Copyright (C) 2017-2025 Hexabitz
  All rights reserved
 
  File Name     : main.c
  Description   : Main program body.
  */
-/* Includes ------------------------------------------------------------------*/
+
+/* Includes ****************************************************************/
 #include "BOS.h"
 
-/* Private variables ---------------------------------------------------------*/
+/* Private variables *******************************************************/
 
-/* Private function prototypes -----------------------------------------------*/
+/* Private Function Prototypes *********************************************/
 
-/* Main function ------------------------------------------------------------*/
+/* Main Function ***********************************************************/
+int main(void){
 
-int main(void) {
+	/* Initialize Module &  BitzOS */
+	Module_Init();
 
-	Module_Init();		//Initialize Module &  BitzOS
-
-	//Don't place your code here.
-	for (;;) {
+	/* Don't place your code here */
+	for(;;){
 	}
 }
 
-/*-----------------------------------------------------------*/
-extern UART_HandleTypeDef huart4;
-/*-----------------------------------------------------------*/
-float adcalue,adcalue4,adcalue2,adcalu3;
-uint8_t k[100];
-uint8_t f ;
+/***************************************************************************/
 /* User Task */
 void UserTask(void *argument){
 
-  // put your code here, to run repeatedly.
-
-
-		while (1) {
-			if (f == 2) {
-						Bridge(P1, P6);
-				Bridge(P2, P3);
-				Bridge(P4, P5);
-				f = 0;
-			}
-			if (f == 1) {
-				Unbridge(P1, P6);
-				Unbridge(P2, P3);
-				Unbridge(P4, P5);
-				f = 0;
-			}
-		}
+	/* put your code here, to run repeatedly. */
+	while(1){
 
 	}
+}
 
-/*-----------------------------------------------------------*/
-/*-----------------------------------------------------------*/
+/***************************************************************************/
+/***************** (C) COPYRIGHT HEXABITZ ***** END OF FILE ****************/
